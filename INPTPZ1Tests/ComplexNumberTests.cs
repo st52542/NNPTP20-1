98b9fbfba4e601038ab7ea673f50a793072cb14a
st52542 <43936282+st52542@users.noreply.github.com>
2020-10-21 01:31:17 +0200

    Add renamed test file

---
+++ INPTPZ1Tests/ComplexNumberTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace INPTPZ1.Mathematics.Tests
+{
+    [TestClass()]
+    public class ComplexNumberTests
+    {
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            ComplexNumber fistNumber = new ComplexNumber()
+            {
+                RealPartOfNumber = 10,
+                ImaginarPartOfNumber = 20
+            };
+            ComplexNumber secondNumber = new ComplexNumber()
+            {
+                RealPartOfNumber = 1,
+                ImaginarPartOfNumber = 2
+            };
+
+            ComplexNumber actual = fistNumber.Add(secondNumber);
+            ComplexNumber shouldBe = new ComplexNumber()
+            {
+                RealPartOfNumber = 11,
+                ImaginarPartOfNumber = 22
+            };
+
+            Assert.AreEqual(shouldBe, actual);
+        }
+    }
+}
+
+
